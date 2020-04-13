b24cc935d9558f6e91f54e76934109936797ce27
aokirisaki <55219463+aokirisaki@users.noreply.github.com>
2020-04-13 11:42:43 +0300

    A

---
+++ Colocviu1_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu13_MainActivity.java
@@ -0,0 +1,24 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Colocviu13_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
